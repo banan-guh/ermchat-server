7e8c3ad76b5d3b2ee6046130ee1bd101bd9484bd
banan <206313076+banan-guh@users.noreply.github.com>
2026-09-01 00:07:42 -0700

    add more handling for input

--- client.go
+++ client.go
@@ -56,16 +56,40 @@ func (c *Client) readPump() {
 			return
 		}
 		line := string(msg)
-		if strings.HasPrefix(line, "JOIN ") {
+		switch {
+		case strings.HasPrefix(line, "CAP REQ "):
+			req := strings.TrimSpace(strings.TrimPrefix(line, "CAP REQ "))
+			c.send <- []byte(":tmi.twitch.tv CAP * ACK " + req + "\r\n")
+
+		case strings.HasPrefix(line, "PASS"):
+			// ignore
+		case strings.HasPrefix(line, "NICK"):
+			nick := strings.TrimSpace(strings.TrimPrefix(line, "NICK "))
+			if nick == "" { nick = "justinfan12345"}
+			c.sendWelcome(nick)
+		case strings.HasPrefix(line, "JOIN "):
 			channel := strings.TrimSpace(strings.TrimPrefix(line, "JOIN "))
 			c.hub.Join(channel, c)
 			c.channels[channel] = true
-		} else if strings.HasPrefix(line, "PART ") {
+
+		case strings.HasPrefix(line, "PART "):
 			channel := strings.TrimSpace(strings.TrimPrefix(line, "PART "))
 			c.hub.Leave(channel, c)
 			delete(c.channels, channel)
 		}
 	}
+}
+
+// header twitch sends (we relay so we need it too)
+func (c *Client) sendWelcome(nick string) {
+    //nick := "justinfan12345"
+    c.send <- []byte(":tmi.twitch.tv 001 " + nick + " :Welcome, GLHF!\r\n")
+    c.send <- []byte(":tmi.twitch.tv 002 " + nick + " :Your host is tmi.twitch.tv\r\n")
+    c.send <- []byte(":tmi.twitch.tv 003 " + nick + " :This server is rather new\r\n")
+    c.send <- []byte(":tmi.twitch.tv 004 " + nick + " :-\r\n")
+    c.send <- []byte(":tmi.twitch.tv 375 " + nick + " :-\r\n")
+	c.send <- []byte(":tmi.twitch.tv 372 " + nick + " :You are in a maze of twisty passages, all alike.\r\n")
+    c.send <- []byte(":tmi.twitch.tv 376 " + nick + " :>\r\n")
 }
 
 func (c *Client) writePump() {
